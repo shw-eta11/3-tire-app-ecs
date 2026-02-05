334518da6b0773d30001873fdea7d0907e42b250
shw-eta11 <shwetarajpara24@gmail.com>
2026-02-05 12:04:14 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@ const db = mysql.createPool({
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  multipleStatements: true
 });
 
 // Wait for DB to be ready
